cd8040087d68072cc5bcc73cba18a8298ec3236b
Cavid112233 <suleymanli535@gmail.com>
2023-11-06 16:26:03 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def bas_diagonal_ortalama_hesapla(A):
+    n = len(A)
+    
+   
+    toplam = 0
+    
+    
+    for i in range(n):
+        toplam += A[i][i]
+    ortalama = toplam / n
+    
+    return ortalama
+A = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+ortalama = bas_diagonal_ortalama_hesapla(A)
+print("Baş diaqonalin ortalaması:", ortalama)
